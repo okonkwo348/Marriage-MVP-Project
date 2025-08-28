d89ac554c565a929be4782c1998d8cd584b87334
okonkwo348 <okonkwoemmanuel348@gmail.com>
2025-08-28 01:59:15 +0100

    Update build.sh to install dependencies with --user and add Profile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn eden_marriage.wsgi:application
--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-pip install -r requirements.txt
+# Install dependencies
+pip install -r requirements.txt --user
+
+# Collect static files and apply migrations
 python manage.py collectstatic --no-input
 python manage.py migrate
