5d09898d50f2009813b34f9ea0b0b8d2f6e8864e
okonkwo348 <okonkwoemmanuel348@gmail.com>
2025-08-28 02:05:10 +0100

    Remove --user from build.sh and ensure all dependencies in requirements.txt

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Install dependencies
-pip install -r requirements.txt --user
+# Install dependencies without --user
+pip install -r requirements.txt
 
 # Collect static files and apply migrations
 python manage.py collectstatic --no-input
